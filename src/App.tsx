3ed12caf3e5c6c6d76a6830fab2eb4033c6ea14b
Ross <dev@rosslibby.com>
2023-12-08 12:06:18 -0500

    Implement Card in main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import Card from 'components/card'
 import './App.css'
+import styles from './app.module.css'
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card
+        action={{
+          label: 'Continue Import',
+          onClick: () => null,
+        }}
+        dismiss={{
+          label: 'Cancel',
+          onClick: () => null,
+        }}
+        summary="Data in the import file is correct. Please press Continue to import."
+        title="Document Upload"
+      >
+        <div className={styles.formLeft}></div>
+
+        <div className={styles.formRight}></div>
+      </Card>
     </div>
   )
 }
