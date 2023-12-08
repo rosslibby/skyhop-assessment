6715b76c33839068cad6cd3cc58fdd5208fc1b3d
Ross <dev@rosslibby.com>
2023-12-08 12:08:12 -0500

    implemented import name dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Card from 'components/card'
 import './App.css'
 import styles from './app.module.css'
+import { Select } from 'components/input'
 
 function App() {
   return (
@@ -17,7 +18,22 @@ function App() {
         summary="Data in the import file is correct. Please press Continue to import."
         title="Document Upload"
       >
-        <div className={styles.formLeft}></div>
+        <div className={styles.formLeft}>
+          <section>
+            <Select
+              name="import-name"
+              onChange={() => null}
+              options={[
+                {
+                  name: 'Select Import Name:',
+                  default: true,
+                  disabled: true,
+                  value: 'none',
+                },
+              ]}
+            />
+          </section>
+        </div>
 
         <div className={styles.formRight}></div>
       </Card>
