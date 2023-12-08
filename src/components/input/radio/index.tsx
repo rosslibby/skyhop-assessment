e87a7dbf60eacfb2b824fe07abe73cbfd504a4a7
Ross <dev@rosslibby.com>
2023-12-08 12:02:42 -0500

    updated radio input group component for layout & styling

--- src/components/input/radio/index.tsx
+++ src/components/input/radio/index.tsx
@@ -19,22 +19,26 @@ export const RadioGroup = ({ label, name, onChange, options }: {
   }
 
   return (
-    <fieldset>
-      <legend>{label}</legend>
-      {options.map((option: RadioOption, index: number) => (
-        <div key={`${name}-radio-${index}`}>
-          <input
-            className={styles.radio}
-            defaultChecked={value === option.value}
-            id={`${name}-${option.value}`}
-            name={name}
-            onChange={handleChange}
-            type="radio"
-            value={option.value}
-          />
-          <label htmlFor={`${name}-${option.value}`}>{option.label}</label>
-        </div>
-      ))}
+    <fieldset className={styles.group}>
+      <legend>
+        <label>{label}</label>
+      </legend>
+      <div className={styles.inputs}>
+        {options.map((option: RadioOption, index: number) => (
+          <div key={`${name}-radio-${index}`}>
+            <input
+              className={styles.radio}
+              defaultChecked={value === option.value}
+              id={`${name}-${option.value}`}
+              name={name}
+              onChange={handleChange}
+              type="radio"
+              value={option.value}
+            />
+            <label htmlFor={`${name}-${option.value}`}>{option.label}</label>
+          </div>
+        ))}
+      </div>
     </fieldset>
   )
 }
