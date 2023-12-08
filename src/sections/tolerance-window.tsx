d4a7ed91a2801471b04a73c1bf848fb23133339f
Ross <dev@rosslibby.com>
2023-12-08 13:31:27 -0500

    implemented toggle switch in tolerance window selector

--- src/sections/tolerance-window.tsx
+++ src/sections/tolerance-window.tsx
@@ -1,9 +1,10 @@
+import { Toggle } from 'components/input/toggle'
+
 export const ToleranceWindow = () => {
   return (
     <section>
       <label htmlFor="tolerance">Tolerance Window:</label>
-      <input type="checkbox" name="tolerance" />
-      <span>{`Toggle ON`}</span>
+      <Toggle name="tolerance" label="Toggle ON" />
       <span>
         <span>Select Tolerance Level</span>
       </span>
