0029c3eb760295eea853c3cc3707b1e2b3945875
Ross <dev@rosslibby.com>
2023-12-08 12:28:04 -0500

    abstracted File Upload section to component

---
+++ src/sections/file-upload.tsx
@@ -0,0 +1,8 @@
+export const FileUpload = () => {
+  return (
+    <section>
+      <label htmlFor="uploader">Select a manifest that you'd like to import</label>
+      <input name="uploader" type="file" />
+    </section>
+  )
+}
--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -1,2 +1,3 @@
 export * from './client'
+export * from './file-upload'
 export * from './split-schedule'
