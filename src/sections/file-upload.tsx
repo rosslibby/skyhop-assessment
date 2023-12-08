48ef9d09d37384e8f3b4414cc0f05929a946e550
Ross <dev@rosslibby.com>
2023-12-08 18:22:40 -0500

    implement file upload section

--- src/sections/file-upload.tsx
+++ src/sections/file-upload.tsx
@@ -1,8 +1,10 @@
+import { File } from 'components/input/file'
+
 export const FileUpload = () => {
   return (
     <section>
       <label htmlFor="uploader">Select a manifest that you'd like to import</label>
-      <input name="uploader" type="file" />
+      <File />
     </section>
   )
 }
