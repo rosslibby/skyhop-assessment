673dc2ef09bd0a62f1abf97c2fc09a22f0238ea0
Ross <dev@rosslibby.com>
2023-12-08 22:04:18 -0500

    defined types for form context

---
+++ src/context/types.ts
@@ -0,0 +1,26 @@
+import { InputValue } from 'types'
+
+export enum FieldType {
+  select,
+  radio,
+  toggle,
+  file,
+}
+
+export type Value = string | boolean | number | File
+
+export interface Field {
+  valid: boolean
+  value?: string | boolean | File | number
+}
+
+export interface Fields {
+  [key: string]: Field
+}
+
+export interface Form {
+  fields: Fields,
+  _: {
+    [key: string]: Function
+  },
+}
