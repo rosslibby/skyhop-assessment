f9257022e154eef1d98026250efd00f0fc7b22f7
Ross <dev@rosslibby.com>
2023-12-08 22:21:03 -0500

    implemented Form context in split-schedule section

--- src/components/input/radio/index.tsx
+++ src/components/input/radio/index.tsx
@@ -1,24 +1,14 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent } from 'react'
 import styles from './radio.module.css'
-import { InputValue, RadioOption } from 'types'
+import { RadioOption } from 'types'
 import { Label } from '../label'
 
 export const RadioGroup = ({ label, name, onChange, options }: {
   label: string
   name: string
-  onChange: (value: InputValue) => void
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
   options: RadioOption[]
 }) => {
-  const [value, setValue] = useState<InputValue | undefined>(
-    options.find((option: RadioOption) => option.checked)?.value
-  )
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-
-    setValue(value)
-    onChange(value)
-  }
-
   return (
     <fieldset className={styles.group}>
       <legend>
@@ -29,10 +19,9 @@ export const RadioGroup = ({ label, name, onChange, options }: {
           <div key={`${name}-radio-${index}`}>
             <input
               className={styles.radio}
-              defaultChecked={value === option.value}
               id={`${name}-${option.value}`}
               name={name}
-              onChange={handleChange}
+              onChange={onChange}
               type="radio"
               value={option.value}
             />
--- src/sections/split-schedule.tsx
+++ src/sections/split-schedule.tsx
@@ -1,10 +1,16 @@
 import { RadioGroup } from 'components/input'
+import { useForm } from 'context/hooks'
+import { ChangeEvent } from 'react'
 
 export const SplitSchedule = () => {
+  const { updateField } = useForm('split-schedule')
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    updateField(e.target.value, e.target.value !== undefined)
+
   return (
     <section>
       <RadioGroup
-        onChange={() => null}
+        onChange={onChange}
         label="Split schedule using social distancing?"
         name="split-schedule"
         options={[
