6964d6bb2b601d24547bec57f21bbef32746d2b8
Ross <dev@rosslibby.com>
2023-12-08 22:09:05 -0500

    add onChange event to toggle component

--- src/components/input/toggle/index.tsx
+++ src/components/input/toggle/index.tsx
@@ -1,13 +1,15 @@
 import styles from './toggle.module.css'
+import { ChangeEvent } from 'react'
 
-export const Toggle = ({ label, name }: {
+export const Toggle = ({ label, name, onChange }: {
   label: string
   name: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.switch}>
-        <input type="checkbox" name={name} />
+        <input type="checkbox" name={name} onChange={onChange} />
         <span className={styles.slider} />
       </label>
       <label htmlFor={name}>{label}</label>
