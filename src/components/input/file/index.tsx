e6f38b8044266c026db692ca4fcda0dfa8ac75b8
Ross <dev@rosslibby.com>
2023-12-09 16:25:56 -0500

    accept props for file upload action; set Material icons

--- src/components/input/file/index.tsx
+++ src/components/input/file/index.tsx
@@ -1,10 +1,14 @@
 import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styles from './file.module.css'
-import { DocumentIcon } from 'assets/icons'
 import { Button } from 'components/buttons'
 
-export const File = () => {
+export const File = ({ action }: {
+  action: {
+    label: string
+    onClick: () => void
+  }
+}) => {
   const onDrop = useCallback((acceptedFiles: File[]) => {}, [])
   const {
     getInputProps,
@@ -17,14 +21,20 @@ export const File = () => {
       <div {...getRootProps()} className={styles.dropzone}>
         <input {...getInputProps()} />
 
-        <DocumentIcon className={styles.icon} />
+        {/* <DocumentIcon className={styles.icon} /> */}
+        <span className={`material-icons ${styles.icon}`}>
+          description
+        </span>
 
         {isDragActive
           ? (<span className={styles.label}>Drop File Here</span>)
           : (<span className={styles.label}>Drag &amp; Drop Here Or <strong>Browse</strong></span>)
         }
       </div>
-      <Button className={styles.button} onClick={() => null}>Upload Manifest</Button>
+      <Button
+        className={styles.button}
+        onClick={action.onClick}
+      >{action.label}</Button>
     </div>
   )
 }
