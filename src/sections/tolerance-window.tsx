8a45f7ebecbde62294bc1d68824138c0e9934ee1
Ross <dev@rosslibby.com>
2023-12-08 22:08:30 -0500

    store tolerance window input value in Form context

--- src/sections/tolerance-window.tsx
+++ src/sections/tolerance-window.tsx
@@ -1,10 +1,21 @@
+import { Label } from 'components/input'
 import { Toggle } from 'components/input/toggle'
+import { useForm } from 'context/hooks'
+import { ChangeEvent } from 'react'
 
 export const ToleranceWindow = () => {
+  const { updateField } = useForm('tolerance-window')
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    updateField(e.target.checked, true)
+
   return (
     <section>
-      <label htmlFor="tolerance">Tolerance Window:</label>
-      <Toggle name="tolerance" label="Toggle ON" />
+      <Label htmlFor="tolerance">Tolerance Window:</Label>
+      <Toggle
+        name="tolerance"
+        label="Toggle ON"
+        onChange={onChange}
+      />
       <span>
         <span>Select Tolerance Level</span>
       </span>
